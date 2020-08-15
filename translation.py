d63bcc58713f761adc16930d08bdc8566e6ebaa3
MY-Tgworld <69492163+MY-Tgworld@users.noreply.github.com>
2020-08-15 12:52:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,18 +1,18 @@
 class Translation(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ I AM USEFUL BOT CAN RENAME FILE AND CONVERT A TELEGRAM FILE TO VIDEO"""
+                     
 
-<b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
-    DOWNLOAD_START = "<B>DOWNLOADING</B>"
-    UPLOAD_START = "<B>UPLOADING</B>"
+    UPGRADE_TEXT = "CONTACT @NOOB_ADMIN"
+    DOWNLOAD_START = "<B>⚡DOWNLOADING⚡</B>"
+    UPLOAD_START = "<B>UPLOADING💝</B>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me **"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
-    NOT_AUTH_USER_TEXT = "CONTACT @prgofficial"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @prgofficial"
+    NOT_AUTH_USER_TEXT = "CONTACT @NOOB_ADMIN"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @noob_admin"
     SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Saved ✅ This Is Permanent"
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
@@ -20,7 +20,7 @@ class Translation(object):
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """ Need my help
     
 1. Send me any Telegram File.
 2. Reply to that message with /rename NEWNAME.EXTENSION
